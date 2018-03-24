33e465348f1ed4598fa99b2bc916dee7e7c0645d
git <cjbrew@gmail.com>
2018-03-24 16:35:35 -0700

    Added a nav bar to the client test page.

--- routes/clientTest.js
+++ routes/clientTest.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.locals = { title: 'Sol' };
+	// Having this page populate the nav bar functionality is bad, should
+	// just wire up the template with the desired client side functions
+	res.locals = { title: 'Sol', mapOnclick : 'alert("hey");' };
 	res.render('clientTest', { partials: { navBar: 'navBar' } });
 });
 
